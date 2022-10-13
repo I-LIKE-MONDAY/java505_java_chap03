a70f7bc76f18bd0df25e3ad107f12cb697894bd3
DESKTOP-4RVPGBS\admin <nmk02@naver.com>
2022-10-13 16:14:26 +0900

    예외처리 전 오류가 발생하는 코드 사용 시

---
+++ src/ExceotionEx.java
@@ -0,0 +1,26 @@
+public class ExceotionEx {
+    public static void main(String[] args) {
+
+//        NullPointException 발생
+//        String data = null;
+//        System.out.println(data.toString());
+
+
+//        ArrayIndexOutOfBoundsException 발생
+//        String data1 = args[0];
+//        String data2 = args[1]; // 인덱스값 없음
+//
+//        System.out.println("args[0] : " + data1);
+//        System.out.println("args[1] : " + data2);
+
+//        NumberFormatException 발생
+//        String data1 = "100";
+//        String data2 = "a100";
+//
+//        int value1 = Integer.parseInt(data1);   // 문자열 -> 정수로
+//        int value2 = Integer.parseInt(data2);   // 오류 발생부분
+//
+//        int result = value1 + value2;
+//        System.out.println(data1 + " + " + data2 + " = " + result);
+    }
+}
